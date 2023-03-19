df8d07d5924e6be8659b4cdae8ca2ac1d142f0a6
hazy-1 <hajime2203@gmail.com>
2023-03-19 22:44:41 +0900

    Delete style files

--- FFTtool/sketch.js
+++ FFTtool/sketch.js
@@ -1,19 +1,18 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-// Create the instance of AudioContext
 let context;
-
-// Create the instance of AnalyserNode
 let analyser;
+
 let spectrum;
 
 let clicked = false;
 
 let hzSize, pointArrayNum1, pointArrayNum2;
-let limit, limitArray;
+let limitDb, limitDbArray;
 let border;
-let minRange, maxRange, rangeSum, rangeAvarage; 
+let minRange, maxRange, rangeSum, rangeAverage; 
 let mindb, maxdb;
+
 let count = 1;
 
 function setup() {
@@ -28,7 +27,7 @@ function setup() {
     border = -70;
 
     // ~13000Hz程度までをFFTする範囲
-    limit = 13000;
+    limitDb = 13000;
 
     // 平均音量を取得する周波数範囲の最小値
     minRange = 6000;
@@ -60,7 +59,7 @@ function setup() {
     
         //band帯を取得
         hzSize = context.sampleRate/analyser.fftSize;
-        limitArray = Math.floor(limit/hzSize);
+        limitDbArray = Math.floor(limitDb/hzSize);
         pointArrayNum1 = Math.floor(minRange/hzSize)-1;
         pointArrayNum2 = Math.floor(maxRange/hzSize)-1;
     });
@@ -76,17 +75,16 @@ function draw() {
         //周波数ごとの音量を取得
         analyser.getFloatFrequencyData(spectrum);
         
-        for (let i = 0; i <= limitArray; i++){
+        for (let i = 0; i <= limitDbArray; i++){
             strokeWeight(2);
             stroke(255);
 
             if(i >= pointArrayNum1 && i <= pointArrayNum2) {
-                console.log(spectrum[i]);
                 rangeSum += spectrum[i];
                 stroke(255, 0, 0);
             }
 
-            let x = map(i, 0, limitArray, 5, width-5);
+            let x = map(i, 0, limitDbArray, 5, width-5);
             let h = map(spectrum[i], -127, 0, height, 0);
             line(x, height, x, h);
 
@@ -97,15 +95,15 @@ function draw() {
         }
         count = 1;
 
-        rangeAvarage = rangeSum/(pointArrayNum2-pointArrayNum1);
-        if(rangeAvarage > maxdb) maxdb = rangeAvarage;
+        rangeAverage = rangeSum/(pointArrayNum2-pointArrayNum1);
+        if(rangeAverage > maxdb) maxdb = rangeAverage;
 
         strokeWeight(50);
         stroke(0, 255, 0)
-        line(width/2, height, width/2, map(rangeAvarage, -127, 0, height, 0));
+        line(width/2, height, width/2, map(rangeAverage, -127, 0, height, 0));
         noStroke();
         fill(255, 0, 0);
-        text(minRange + "~" + maxRange + "Average:" + rangeAvarage, 20, 20);
+        text(minRange + "~" + maxRange + "Average:" + rangeAverage, 20, 20);
 
         textAlign(RIGHT);
         text("maxHz:" + maxdb, width-20, 20);
@@ -113,8 +111,5 @@ function draw() {
         stroke(255);
         strokeWeight(2);
         line(0,  map(border, -127, 0, height, 0), width,  map(border, -127, 0, height, 0));
-    }
-    else {
-        background(0);
     }
 }
